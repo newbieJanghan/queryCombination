c529f368f40d0c97408c7d98f3291a85fb6aa69a
Janghan Bae <102276240+newbieJanghan@users.noreply.github.com>
2022-07-01 13:58:05 +0900

    Create union textScore with regex.js

---
+++ union textScore with regex.js
@@ -0,0 +1,27 @@
+aync findBySearch(filter {
+  const result = await Product.aggregate([
+      { $match: { $text: { $search: filter } } },
+      { $addFields: { score: { $meta: 'textScore' } } },
+      { $unionWith: {
+          coll: 'products',
+          pipeline: [
+            { $match: {
+                $or: [
+                  { field1: { $regex: `${filter}` } },
+                  { field2: { $regex: `${filter}` } },
+                  { field3_array: { $elemMatch: { $regex: `${filter}` } } },
+                ],
+            }},
+            { $addFields: { score: 1 } },
+          ],
+      }},
+      { $sort: { score: -1 } },
+      { $group: {
+          _id: '$_id',
+          fiekd1: { $first: '$field1' },
+          ...,
+          score: { $sum: '$score' },
+      }},
+    ]);
+  return product;
+}
